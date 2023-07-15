008cd47d63a5c021135c7da32be726b608650b27
yurii <benisones@gmail.com>
2023-07-15 16:15:31 +0300

    reload

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import os
+from faker import Faker
+
+
+def show_greeting() -> [str]:
+    fake = Faker()
+    name = fake.name()
+    address = fake.address()
+
+    print(f"Hello, my name is {name}!\nI live in {address}\n")
+
+
+def show_current_directory() -> [str]:
+    current_directory = os.getcwd()
+    print(f"We work in {current_directory}")
+
+
+def main():
+    show_greeting()
+    show_current_directory()
