687bcc1d7e3e08c89ec085af6143ff4ef34bf12f
yurii <benisones@gmail.com>
2023-07-18 16:11:15 +0300

    some fixes

--- app/services/parth_json.py
+++ app/services/parth_json.py
@@ -1,20 +1,15 @@
 import requests
 import json
 
-
-
 def read_file(link="http://api.open-notify.org/astros.json")->str:
     response = requests.get(link).text
     return response
-
-
 
 def convert_json(file=read_file())->json:
     result = json.loads(file)
     return result
 
 def save_astronauts(file=convert_json()):
-    astronauts_number = file['number']
     astronauts = file['people']
 
     for astronaut in astronauts:
@@ -22,7 +17,7 @@ def save_astronauts(file=convert_json()):
         craft = astronaut['craft']
         yield name, craft
 
-def show_astronauts(astronauts= save_astronauts()):
+def show_astronauts(astronauts=save_astronauts()):
     astronauts_list = []
     for astronaut in astronauts:
         astronaut_str = ': '.join(astronaut)
@@ -30,5 +25,4 @@ def show_astronauts(astronauts= save_astronauts()):
         astronauts_list.append(astronaut_reformat)
 
     result = ''.join(astronauts_list)
-    text = f'<b>In space we have 10 astronauts</b>'
-    return f"{text}<br><ul>{result}</ul>"
+    return f"<b>In space we have 10 astronauts</b><br><ul>{result}</ul>"
