f4701096cd99ed35bf36f0b1801ce375ebb1d506
yurii <benisones@gmail.com>
2023-08-02 14:17:53 +0300

    some fix

--- application/services/show_average.py
+++ application/services/show_average.py
@@ -5,32 +5,36 @@ from application.config import FILES_OUTPUT_DIR
 
 
 
-def file_read(FILEPATH=FILES_OUTPUT_DIR.joinpath('people_data(extended).csv')):
-    with open(FILEPATH, 'r') as csvfile:
+def file_read(file_path=None):
+    if file_path is None:
+        file_path = FILES_OUTPUT_DIR.joinpath('people_data(extended).csv')
+    with open(file_path, 'r') as csvfile:
         reader = list(csv.DictReader(csvfile))
 
         return reader
 
-def find_average_height(file=None)->float:
-    file = file_read()
+def find_average_height(csv_file=None)->float:
+    if csv_file is None:
+        csv_file = file_read()
     height_sum = 0
 
-    for row in file:
+    for row in csv_file:
         height_sum += float(row['Height(Inches)'])
 
 
-    return height_sum / len(file)
+    return height_sum / len(csv_file)
 
 
 
-def find_average_weight(file=None)->float:
-    file = file_read()
+def find_average_weight(csv_file=None)->float:
+    if csv_file is None:
+        csv_file = file_read()
     weight_sum = 0
 
-    for row in file:
+    for row in csv_file:
         weight_sum += float(row['Weight(Pounds)'])
 
-    return weight_sum / len(file)
+    return weight_sum / len(csv_file)
 
 def convert_weight(weight:float = find_average_weight())->float:
     return weight  * 0.453592
